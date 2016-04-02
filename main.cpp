eb2ef77be3816124850c8b6e1902b36a7f92ee5c
HeuiWoong <bisk5656@jungbu.ac.kr>
2016-04-02 12:18:38 -0700

    add print src/dst ip_addr and protocol

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <arpa/inet.h>
 #include <pcap.h>
 #include <net/ethernet.h>
-#include <linux/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <iostream>
 
 int main(/*int argc, char *argv[]*/)
 {
@@ -30,30 +32,44 @@ int main(/*int argc, char *argv[]*/)
     while(1){
         const u_char *p;
         struct pcap_pkthdr *h;
-        struct ether_header *header;
+        struct ether_header *eth_header;
+        struct iphdr *ip_header;
         int res = pcap_next_ex(handle, &h, &p);
 
-        header = (struct ether_header*) p;
-        unsigned int ptype = ntohs(header->ether_type);
+        eth_header = (struct ether_header*) p;
+        ip_header = (struct iphdr*)(p+sizeof(struct ether_header));
+        unsigned int ptype = ntohs(eth_header->ether_type);
 
         if(res == -1) break;
         if(res == 1){
-            printf("Jacked a packet %p with length of [%d]%x\n", p, h->caplen, *p);
-            printf("dst mac : [%02x:%02x:%02x:%02x:%02x:%02x] ",
-                   header->ether_dhost[0],
-                    header->ether_dhost[1],
-                    header->ether_dhost[2],
-                    header->ether_dhost[3],
-                    header->ether_dhost[4],
-                    header->ether_dhost[5]);
-            printf("src mac : [%02x:%02x:%02x:%02x:%02x:%02x] ",
-                   header->ether_shost[0],
-                    header->ether_shost[1],
-                    header->ether_shost[2],
-                    header->ether_shost[3],
-                    header->ether_shost[4],
-                    header->ether_shost[5]);
-            printf("protocol type : %04x\n", ptype);
+            if(ptype == 2048){
+                printf("Jacked a packet %p with length of [%d]%x\n", p, h->len, *p);
+                printf("dst mac : [%02x:%02x:%02x:%02x:%02x:%02x] ",
+                       eth_header->ether_dhost[0],
+                        eth_header->ether_dhost[1],
+                        eth_header->ether_dhost[2],
+                        eth_header->ether_dhost[3],
+                        eth_header->ether_dhost[4],
+                        eth_header->ether_dhost[5]);
+                printf("src mac : [%02x:%02x:%02x:%02x:%02x:%02x] ",
+                       eth_header->ether_shost[0],
+                        eth_header->ether_shost[1],
+                        eth_header->ether_shost[2],
+                        eth_header->ether_shost[3],
+                        eth_header->ether_shost[4],
+                        eth_header->ether_shost[5]);
+                printf("protocol type : %04x\n", ptype);
+                printf("src ip : %s\n", inet_ntoa(*(struct in_addr *)&ip_header->saddr));
+                printf("dst ip : %s\n", inet_ntoa(*(struct in_addr *)&ip_header->daddr));
+
+                switch(ip_header->protocol){
+                    case 6: {printf("protocol : TCP\n"); break;};
+                    case 17: {printf("protocol : UDP\n"); break;};
+                    default: {printf("protocol : %d\n", ip_header->protocol); break;};
+                }
+            }
+
+            else printf("no ip protocol\n");
         }
     }
 
