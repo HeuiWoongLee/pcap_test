74d439762beb42c2b55035703ff12c081a47035e
HeuiWoong <bisk5656@jungbu.ac.kr>
2016-04-02 10:16:49 -0700

    add print dst/src mac and ptype

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <arpa/inet.h>
 #include <pcap.h>
+#include <net/ethernet.h>
+#include <linux/if_ether.h>
 
 int main(/*int argc, char *argv[]*/)
 {
@@ -25,12 +28,33 @@ int main(/*int argc, char *argv[]*/)
     }
 
     while(1){
+        const u_char *p;
         struct pcap_pkthdr *h;
-        const u_char * p;
+        struct ether_header *header;
         int res = pcap_next_ex(handle, &h, &p);
 
+        header = (struct ether_header*) p;
+        unsigned int ptype = ntohs(header->ether_type);
+
         if(res == -1) break;
-        if(res == 1) printf("Jacked a packet %p with length of [%d]%x\n", p, h->caplen, *p);
+        if(res == 1){
+            printf("Jacked a packet %p with length of [%d]%x\n", p, h->caplen, *p);
+            printf("dst mac : [%02x:%02x:%02x:%02x:%02x:%02x] ",
+                   header->ether_dhost[0],
+                    header->ether_dhost[1],
+                    header->ether_dhost[2],
+                    header->ether_dhost[3],
+                    header->ether_dhost[4],
+                    header->ether_dhost[5]);
+            printf("src mac : [%02x:%02x:%02x:%02x:%02x:%02x] ",
+                   header->ether_shost[0],
+                    header->ether_shost[1],
+                    header->ether_shost[2],
+                    header->ether_shost[3],
+                    header->ether_shost[4],
+                    header->ether_shost[5]);
+            printf("protocol type : %04x\n", ptype);
+        }
     }
 
     return(0);
